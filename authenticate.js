24b8af90b6591be178245d2660639311db5830fe
Michael Wood <michaelwood33311@icloud.com>
2023-02-18 08:41:38 -0500

    added creating login table

--- authenticate.js
+++ authenticate.js
@@ -1,3 +1,29 @@
 const mysql = require('./utils/mysql');
 
-console.log(mysql);
+const createLoginTable = async () => {
+    const q = `CREATE TABLE IF NOT EXISTS login(
+        user_id VARCHAR(40) NOT NULL,
+        user_name VARCHAR(512) NOT NULL,
+        email VARCHAR(325) NOT NULL,
+        password VARCHAR(512) NOT NULL,
+        token VARCHAR(40000) NOT NULL,
+        PRIMARY KEY (user_id),
+        UNIQUE KEY (user_name),
+        UNIQUE KEY (email)
+    )`;
+
+    return mysql.query(q);
+}
+
+const launchService = async () => {
+    console.log('launchService');
+
+    await createLoginTable();
+}
+
+let waitId = setInterval(() => {
+    if (mysql.sqlReady) {
+        clearInterval(waitId);
+        launchService();
+    }
+}, 1000);
